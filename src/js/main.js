e5810a468ca5f1f9d1974b9a0f8eb48974f46811
VladislavVB <burdukovskij.vlad@mail.ru>
2019-12-25 21:21:00 +0300

    add: Dist & fix gulp

---
+++ src/js/main.js
@@ -0,0 +1,220 @@
+/* js
+document.addEventListener("DOMContentLoaded", function (event) {
+  const modal = document.querySelector('.modal');
+  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+  const closeBtn = document.querySelector('.modal__close');
+  const switchModal = () => {
+    modal.classList.toggle('modal--visible');
+  }
+
+  modalBtn.forEach(element => {
+    element.addEventListener('click', switchModal);
+  });
+
+  closeBtn.addEventListener('click', switchModal);
+
+  document.addEventListener('keydown', function (e) {
+    if (e.keyCode === 27) {switchModal() }
+  });
+
+  document.addEventListener('click', function(e) {
+    e.target.classList.toggle('.modal--visible')
+    });
+  
+  });
+*/
+
+// jquery
+$(document).ready(function () {
+ 
+  var modal = $('.modal'),
+    modalBtn = $('[data-toggle=modal]'),
+    closeBtn = $('.modal__close');
+  
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+
+  $(document).keydown(function () {
+    if (event.keyCode == 27) {
+      modal.removeClass('modal--visible');
+    }
+  }); 
+
+  $(document).click(function (event) {
+    $(event.target).toggleClass('modal--visible');
+  });
+
+  $(function(){
+	$(window).scroll(function(){
+  	if ($(document).scrollTop()>$(window).height()){
+    	 $('.scroll-to-top').show();
+    } else{
+    	$('.scroll-to-top').hide();
+    }
+  });
+  $('.scroll-to-top').click(function(){
+  	$('html,body').animate({scrollTop: 0}, 1000);
+  });
+});
+
+$('#offer-form').on('submit', function name(event) {
+  event.preventDefault();
+  $.ajax({
+    type: "POST",
+    url: "send.php",
+    datd: $(this).serialize(),
+    success: function (response) {
+      console.log('Прибыли данные: ' + response);
+      $('#offer-form')[0].reset();
+      modal.classList.add('modal_active');
+      $(".modal-dialog__title").text(response);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.error(jqXHR + " " + textStatus);
+      
+    }
+    
+  });
+})
+// slider
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 20 + bullets.width() + 20)
+  bullets.css('left', prev.width() + 20)
+
+  new WOW().init();
+
+  //Валидация форма
+  $('.modal__form').validate({
+    errorClass: "invalid",
+    errorElement: "div",
+    rules: {
+      // строчное правило
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlenght: 15 
+      },
+      userPhone: "required",
+      // правило-объект (блок)
+      userEmail: {
+        required: true,
+        email: true
+    }
+  }, // сообщения
+  messages: {
+    userName: {
+      required: "Имя обязательно для заполнения",
+      minlength: "Имя не короче 2-х букв"
+    },
+    userPhone: "Телефон обязателен для заполнения",
+    userEmail: {
+      required: "Обязательно укажите Email",
+      email: "Введите в формате: name@domain.com"
+    }
+  }
+  });
+
+  $('.control__form').validate({
+    errorClass: "invalid",
+    errorElement: "div",
+    rules: {
+      // строчное правило
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlenght: 15,
+      },
+      userPhone: "required",
+  }, // сообщения
+  messages: {
+    userName: {
+      required: "Имя обязательно для заполнения",
+      minlength: "Имя не короче 2-х букв"
+    },
+    userPhone: "Телефон обязателен для заполнения",
+  }
+  });
+
+   $('.footer__form').validate({
+    errorClass: "invalid",
+    errorElement: "div",
+    rules: {
+      // строчное правило
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlenght: 15, 
+      },
+      userPhone: "required",
+      userQuestion: "required",
+      
+  }, // сообщения
+  messages: {
+   userName: {
+      required: "Имя обязательно для заполнения",
+      minlength: "Имя не короче 2-х букв"
+    },
+    userPhone: "Телефон обязателен для заполнения",
+    userQuestion: "Пожалуйста, напишите Ваш вопрос",
+  }
+  });
+
+  
+  //маска для номера телефона
+
+  $('[type=tel]').mask('+7(000) 000-00-00', { placeholder: "+7(___) ___-__-__" });
+  
+  // создание yandex карты
+ ymaps.ready(function () {
+   var myMap = new ymaps.Map('map', {
+     center: [47.244734, 39.723227],
+     zoom: 15
+   }, {
+     searchControlProvider: 'yandex#search'
+   }),
+
+     // Создаём макет содержимого.
+     MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+       '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+     ),
+
+     myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+       hintContent: 'Наш офис',
+       balloonContent: 'Вход со двора'
+     }, {
+       // Опции.
+       // Необходимо указать данный тип макета.
+       iconLayout: 'default#image',
+       // Своё изображение иконки метки.
+       iconImageHref: 'img/mapIcon.png',
+       // Размеры метки.
+       iconImageSize: [32, 32],
+       // Смещение левого верхнего угла иконки относительно
+       // её "ножки" (точки привязки).
+      iconImageOffset: [-5, -38]
+     });
+
+    myMap.geoObjects
+        .add(myPlacemark);
+});
+
+});
---
+++ src/send.php
@@ -0,0 +1,43 @@
+<?php
+
+$userName = $_POST['userName'];
+$userPhone = $_POST['userPhone'];
+$userEmail = $_POST['userEmail'];
+$userQuestion = $_POST['userQuestion'];
+
+// Load Composer's autoloader
+require 'phpmailer/Exception.php';
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.yandex.ru';                       // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'vladburdukovsky@yandex.ru';               // SMTP username
+    $mail->Password   = '89385143014';                        // SMTP password
+    $mail->SMTPSecure = 'ssl';            // Enable TLS encryption; 
+    $mail->Port       = 465;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('vladburdukovsky@yandex.ru', 'Владислав');
+    $mail->addAddress('vlad.burdukoasky@yandex.ru');     // Add a recipient
+    
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = "New order";
+    $mail->Body    = "Имя пользователя ${userName}, его телефон: ${userPhone}. Его почта: ${userEmail} Его вопрос: ${userQuestion}";
+    //проверяяем отправленность сообщения
+if ($mail->send()) {
+   echo: "Форма успешно отправлена";
+} else {
+    echo "Письмо не отправлено, неверная почта ";
+}
+} catch (Exception $e) {
+    echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+}
