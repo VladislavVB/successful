352d8e9c200eccc4bdd7d0c3e2e53b7bd2ca07a4
VladislavVB <burdukovskij.vlad@mail.ru>
2019-12-27 20:21:08 +0300

    php

--- src/send.php
+++ src/send.php
@@ -1,38 +1,54 @@
 <?php
+
 $userName = $_POST['userName'];
 $userPhone = $_POST['userPhone'];
 $userEmail = $_POST['userEmail'];
-$userQuestion = $_POST['userQuestion'];
+$customerName = $_POST['customerName'];
+$customerPhone = $_POST['customerPhone'];
+$clientName = $_POST['clientName'];
+$clientPhone = $_POST['clientPhone'];
+$clientQuestion = $_POST['clientQuestion'];
+
 // Load Composer's autoloader
 require 'phpmailer/Exception.php';
 require 'phpmailer/PHPMailer.php';
 require 'phpmailer/SMTP.php';
+
 // Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer\PHPMailer\PHPMailer();
+$mail->CharSet = 'utf-8';
+
 try {
     //Server settings
     $mail->SMTPDebug = 0;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.yandex.ru';                       // Set the SMTP server to send through
+    $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'vladburdukovsky@yandex.ru';               // SMTP username
-    $mail->Password   = '89385143014';                        // SMTP password
+    $mail->Username   = 'burdukoaskijvladislav@gmail.com';               // SMTP username
+    $mail->Password   = '89385143014aa';                        // SMTP password
     $mail->SMTPSecure = 'ssl';            // Enable TLS encryption; 
     $mail->Port       = 465;                                    // TCP port to connect to
+
     //Recipients
-    $mail->setFrom('vladburdukovsky@yandex.ru', 'Владислав');
-    $mail->addAddress('vlad.burdukoasky@yandex.ru');     // Add a recipient
+    $mail->setFrom('burdukoaskijvladislav@gmail.com', 'Vladislav');
+    $mail->addAddress('burdukovskij.vlad@mail.ru');     // Add a recipient
     
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = "New order";
     $mail->Body    = "Имя пользователя ${userName}, его телефон: ${userPhone}. Его почта: ${userEmail} Его вопрос: ${userQuestion}";
-    //проверяяем отправленность сообщения
-if ($mail->send()) {
-   echo: "Форма успешно отправлена";
-} else {
-    echo "Письмо не отправлено, неверная почта ";
-}
-} catch (Exception $e) {
-    echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
-}
+    
+    $mail->send();
+    // header('Location: index.html');
+
+// Проверяем результат отправки сообщения
+    if ($mail->send()) {
+        echo "Форма успешно отправлена";
+    } else {
+        echo "Сообщение не был отправлено. Неверно указаны настройки вашей почты";
+    }
+
+    } catch (Exception $e) {
+        echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+    }
+?>
